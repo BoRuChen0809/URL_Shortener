9247c4121205565134791d7efe1a5e1036309ff5
BoRu Chen <chenboru0124@gmail.com>
2022-03-09 19:34:55 +0800

    router

--- router/api/v1/MyURL.go
+++ router/api/v1/MyURL.go
@@ -1,35 +1,38 @@
 package v1
 
 import (
+	"URL_Shortener/logic"
+	"URL_Shortener/model"
+	"URL_Shortener/model/mysql"
+	"URL_Shortener/model/redis"
+	my_hashids "URL_Shortener/package/hashids"
 	"fmt"
 	"net/http"
 	"time"
 
 	"github.com/gin-gonic/gin"
-
-	"URL_Shortener/global"
-	"URL_Shortener/model/mysql"
-	mypkg "URL_Shortener/package"
+	"gorm.io/gorm"
 )
 
 func AddURL(c *gin.Context) {
 	m := make(map[string]interface{})
 
 	//JSON轉map
-	if err := c.ShouldBindJSON(&m); err != nil {
+	if err := c.BindJSON(&m); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"message": "參數解析錯誤"})
 		return
 	}
 
 	//處理url
 	url := fmt.Sprint(m["url"])
-	if url == "" {
-		c.JSON(http.StatusBadRequest, gin.H{"message": "url解析錯誤"})
+	if url == "" || !logic.VerifyURL(url) {
+		c.JSON(http.StatusBadRequest, gin.H{"message": "url錯誤"})
 		return
 	}
 
 	//處理expire_at
-	expire_at, err := time.Parse(time.RFC3339, fmt.Sprint(m["expireAt"]))
+	str_time := fmt.Sprint(m["expireAt"])
+	expire_at, err := time.Parse(time.RFC3339, str_time)
 	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"message": "expireAt解析錯誤"})
 		return
@@ -39,35 +42,68 @@ func AddURL(c *gin.Context) {
 		return
 	}
 
-	//寫入Mysql
-	id, err := mysql.Insert(expire_at, url)
-	if err != nil {
+	//資料庫讀寫
+	var hash_id string
+	my_url, mysql_read_err := mysql.ReadByURL(url)
+	if mysql_read_err != gorm.ErrRecordNotFound && mysql_read_err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"message": "發生內部錯誤"})
 		return
+	} else if mysql_read_err == gorm.ErrRecordNotFound {
+		my_url := model.MyURL{URL: url, ExpireAt: expire_at}
+		id, err := mysql.Create(my_url)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"message": "發生內部錯誤"})
+			return
+		}
+		hash_id, err = my_hashids.NewHashID(id)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"message": "發生內部錯誤"})
+			return
+		}
+		err = redis.SetURL(hash_id, url, str_time)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"message": "發生內部錯誤"})
+			return
+		}
+	} else {
+		hash_id, err = my_hashids.NewHashID(my_url.ID)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"message": "發生內部錯誤"})
+			return
+		}
+		if my_url.ExpireAt.Before(expire_at) {
+			my_url.ExpireAt = expire_at
+			err := mysql.Update(*my_url)
+			if err != nil {
+				c.JSON(http.StatusInternalServerError, gin.H{"message": "發生內部錯誤"})
+				return
+			}
+			err = redis.UpdateURL(hash_id, str_time)
+			if err != nil {
+				c.JSON(http.StatusInternalServerError, gin.H{"message": "發生內部錯誤"})
+				return
+			}
+		}
 	}
 
-	//編碼、回傳結果
-	str_id := mypkg.ByteArray2String(global.Encode.Uint2ByteArray(id))
-	short_url := fmt.Sprintf("http://localhost:8080/%s", str_id)
-	c.JSON(http.StatusOK, gin.H{"id": str_id, "short_url": short_url})
+	//回傳結果
+	short_url := fmt.Sprintf("http://localhost:8080/%s", hash_id)
+	c.JSON(http.StatusOK, gin.H{"id": hash_id, "short_url": short_url})
+
 }
 
 func GetURL(c *gin.Context) {
 	str_id := c.Param("id")
+	url, err := redis.GetURL(str_id)
 
-	//解碼
-	id, err := global.Encode.ByteArray2Uint(mypkg.String2ByteArray(str_id))
 	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"code": http.StatusBadRequest, "message": "解析url_id錯誤"})
+		if err.Error() == "redigo: nil returned" {
+			c.JSON(http.StatusNotFound, gin.H{"message": "連結不存在或已過期"})
+			return
+		}
+		c.JSON(http.StatusInternalServerError, gin.H{"message": "發生內部錯誤"})
 		return
 	}
 
-	//查詢Mysql
-	myurl, err := mysql.SelectByID(id)
-	if err != nil {
-		c.JSON(http.StatusNotFound, gin.H{"message": "連結不存在或已過期"})
-		return
-	}
-
-	c.Redirect(http.StatusMovedPermanently, myurl.URL)
+	c.Redirect(http.StatusMovedPermanently, url)
 }
--- router/router.go
+++ router/router.go
@@ -2,17 +2,15 @@ package router
 
 import (
 	v1 "URL_Shortener/router/api/v1"
-	v2 "URL_Shortener/router/api/v2"
 
 	"github.com/gin-gonic/gin"
 )
 
 func NewRouter() *gin.Engine {
 	r := gin.Default()
-	r.GET("/mysql/:id", v1.GetURL)
-	r.POST("/api/mysql/urls", v1.AddURL)
 
-	r.GET("/:id", v2.GetURL)
-	r.POST("/api/v1/urls", v2.AddURL)
+	r.GET("/:id", v1.GetURL)
+	r.POST("/api/v1/urls", v1.AddURL)
+
 	return r
 }
