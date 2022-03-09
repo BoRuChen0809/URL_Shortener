f505e1e74fc6f062203732db0e9715ec282538e9
BoRu Chen <chenboru0124@gmail.com>
2022-03-09 19:33:27 +0800

    logic

---
+++ logic/url_verification.go
@@ -0,0 +1,35 @@
+package logic
+
+import (
+	"net/http"
+	"net/url"
+)
+
+func VerifyURL(path string) bool {
+	u, err := url.Parse(path)
+	if err != nil {
+		return false
+	}
+	switch u.Host {
+	case "":
+		return false
+	case "localhost:8080":
+		return false
+	}
+
+	req, err := http.NewRequest(http.MethodGet, path, nil)
+	if err != nil {
+		return false
+	}
+	c := &http.Client{}
+	res, err := c.Do(req)
+	if err != nil {
+		return false
+	}
+	defer res.Body.Close()
+	if res.StatusCode > 299 || res.StatusCode < 200 {
+		return false
+	}
+
+	return true
+}
